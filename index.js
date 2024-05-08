afd85afaf67917538fc07361b2a93459291380c4
alekseygurenkov <aleksey.sknt@gmail.com>
2024-05-08 21:06:46 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Розовый котик просто красота';
+});
